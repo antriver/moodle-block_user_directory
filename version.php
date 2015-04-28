6f6ec9fc2496f182555bd1bbad8946903c1d41d0
Anthony Kuske <anthonykuske@gmail.com>
2015-04-28 20:55:01 +0200

    Update required moodle version

--- version.php
+++ version.php
@@ -10,6 +10,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015042800; // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2012112900; // Requires this Moodle version
+$plugin->version = 2015042800; // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires = 2013111800; // MOODLE_26_STABLE
 $plugin->component = 'block_user_directory'; // Full name of the plugin (used for diagnostics)
